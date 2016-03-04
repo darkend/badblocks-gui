038137bb8977da68acb77e9061dccf58aba585a4
Walter Casañas <sede.lisa@gmail.com>
2016-03-04 11:24:47 -0500

    Added files via upload

--- badblocks.py
+++ badblocks.py
@@ -2,25 +2,48 @@
 
 # -*- coding: utf-8 -*-
 
-import os, sys
+# Reparador de Discos Duros
+
+import sys
+import os
+
 import subprocess
 import commands
+from PyQt4 import QtCore, QtGui, uic
 
-#### Hacemos fdisk -l ####
+# Cargar nuestro archivo .ui
+form_class = uic.loadUiType("badblocks-gui.ui")[0]
 
+### Hacemos fdisk -l e imprimimos resulatdos en pantalla ####
 fdisk = commands.getoutput("sudo fdisk -l")
 print fdisk
 
-### Solicitamos el dispositivo a reparar ###
-
+### Solicitamos el dispositivo a reparar. Omitimos el /dev/ ###
 dispositivo = raw_input ("introdusca el dispositivo a reparar.(sda, sdc, omita el /dev/)")
-
 dev = dispositivo
 
+# Preguntamos si desea hacerlo en modo destructivo.
+destructivo = raw_input ("Desea hacerlo en Modo destructivo S/N")
+d = destructivo
 
-### Declaramos el comando a utilizar
+### Declaramos el comando a utilizar. Modo no destructivo.
+repararhd = os.system("sudo badblocks -v -n -s -f /dev/" + dev)
 
-repararhd = os.system("sudo badblocks -v -n -f /dev/" + dev)
+### Declaramos el comando a utilizar. Modo destructivo.
+repararhdd = os.system("sudo badblocks -v -w -s -f /dev/" + dev)
 
-print repararhd
+#Declaramos condicional
+if d == N:
+	print repararhdd
+else: 
+	print repararhd
+
+
+
+
+
+
+
+
+
 
